67ad32714368f41da8968dda292df56025549a15
Matias <eluneymerillan@gmail.com>
2025-02-25 20:28:22 -0300

    Agregar entidades

---
+++ src/main/java/com/egg/demo/entidades/Libro.java
@@ -0,0 +1,96 @@
+package com.egg.demo.entidades;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@Entity
+public class Libro {
+
+    @Id
+    private Long isbn;
+
+    private String titulo;
+
+    private int ejemplares;
+
+    @Temporal(TemporalType.DATE)
+    private Date alta;
+
+    @ManyToOne
+    @JoinColumn(name = "autor_id")
+    private Autor autor;
+
+    @ManyToOne
+    @JoinColumn(name = "editorial_id")
+    private Editorial editorial;
+
+    public Libro() {
+    }
+
+    public Libro(Long isbn, String titulo, int ejemplares, Date alta) {
+        this.isbn = isbn;
+        this.titulo = titulo;
+        this.ejemplares = ejemplares;
+        this.alta = alta;
+    }
+
+    public Long getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(Long isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getEjemplares() {
+        return ejemplares;
+    }
+
+    public void setEjemplares(int ejemplares) {
+        this.ejemplares = ejemplares;
+    }
+
+    public Date getAlta() {
+        return alta;
+    }
+
+    public void setAlta(Date alta) {
+        this.alta = alta;
+    }
+
+    @Override
+    public String toString() {
+        return "Libro [isbn=" + isbn + ", titulo=" + titulo + ", ejemplares=" + ejemplares + ", alta=" + alta + "]";
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+
+    public Editorial getEditorial() {
+        return editorial;
+    }
+
+    public void setEditorial(Editorial editorial) {
+        this.editorial = editorial;
+    }
+
+}
