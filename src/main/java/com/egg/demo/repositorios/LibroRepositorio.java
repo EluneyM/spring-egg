8b7cd2fc3d09847eb16bc6ce01b7234bc7aac4e1
Matias <eluneymerillan@gmail.com>
2025-02-25 20:32:56 -0300

    Agregando consulta en el repositorio de libro

--- src/main/java/com/egg/demo/repositorios/LibroRepositorio.java
+++ src/main/java/com/egg/demo/repositorios/LibroRepositorio.java
@@ -1,11 +1,15 @@
 package com.egg.demo.repositorios;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.egg.demo.entidades.Libro;
 
 @Repository
-public interface LibroRepositorio extends JpaRepository<Libro, Long>{
+public interface LibroRepositorio extends JpaRepository<Libro, Long> {
 
+    @Query("SELECT l FROM Libro l WHERE l.titulo = :titulo")
+    public Libro buscarPorTitulo(@Param("titulo") String titulo);
 }
