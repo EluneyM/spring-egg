247cd07af1056f4ce304c101546194b3d9882ae6
Matias <eluneymerillan@gmail.com>
2025-02-26 20:54:29 -0300

    Faltaba la notation repository

--- src/main/java/com/egg/demo/repositorios/EditorialRepositorio.java
+++ src/main/java/com/egg/demo/repositorios/EditorialRepositorio.java
@@ -1,9 +1,11 @@
 package com.egg.demo.repositorios;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.egg.demo.entidades.Editorial;
 
+@Repository
 public interface EditorialRepositorio extends JpaRepository<Editorial, Long> {
 
 }
