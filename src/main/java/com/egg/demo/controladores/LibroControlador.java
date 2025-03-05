fd5502cf79ca9d8d9e3e3166c90ac3ef4099b129
Matias <eluneymerillan@gmail.com>
2025-03-05 20:59:26 -0300

    Listar libros

--- src/main/java/com/egg/demo/controladores/LibroControlador.java
+++ src/main/java/com/egg/demo/controladores/LibroControlador.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.egg.demo.entidades.Autor;
 import com.egg.demo.entidades.Editorial;
+import com.egg.demo.entidades.Libro;
 import com.egg.demo.servicios.AutorServicio;
 import com.egg.demo.servicios.EditorialServicio;
 import com.egg.demo.servicios.LibroServicio;
@@ -58,5 +59,13 @@ public class LibroControlador {
             return "libro_form.html";
         }
         return "index.html";
+    }
+
+    @GetMapping("/lista")
+    public String listar(ModelMap modelo) {
+
+        List<Libro> libros = libroServicio.listarLibros();
+        modelo.addAttribute("libros", libros);
+        return "libro_list.html";
     }
 }
